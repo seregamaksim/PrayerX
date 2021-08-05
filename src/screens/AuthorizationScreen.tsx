7085a93a8df50672a46465d214cab19eb1e096ff
Sergey Maksimenko <serega.maksimenko@gmail.com>
2021-08-05 22:03:56 +0600

    install dependencies

---
+++ src/screens/AuthorizationScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+export default function AuthorizationScreen() {
+
+    return (
+        <View>
+            <Text>Authorization screen</Text>
+        </View>
+    )
+}
